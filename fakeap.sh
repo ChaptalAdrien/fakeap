d4184723bfbfe6267d063f65d3b658b2eaa7028c
Mob <adrien.chaptal@gmail.com>
2019-12-16 18:54:54 +0100

    modif version tuto ubuntu

--- fakeap.sh
+++ fakeap.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
-# Start
 
-#Assign the network Gateway and netmask to the interface and add the routing table
-ifconfig wlan0 up 192.168.1.1 netmask 255.255.255.0
-route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1
+#interfaces configuration
+INT_WIFI="wlan0" # wifi access point interface
+INT_NET="eth0" # interface connected to internet
 
-# Start DHCP/DNS server
-dnsmasq -C conf/dnsmasq.conf -H conf/fakehosts.conf -d
+#Ip & mask of the wlan network
+SUBNET="192.168.0.0/24" 
+IP="192.168.0.1"
+MASK="255.255.255.0"
 
-# Enable routing
-sysctl net.ipv4.ip_forward=1
+echo -e "WIFI interface configuration ..."
+sudo ifconfig $INT_WIFI down
+sleep 0.5
+sudo ifconfig $INT_WIFI $IP netmask $MASK up
 
-# Enable NAT
-iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+echo -e "startintg daemon hostapd..."
+# start hostapd server (see hostapd.conf)
+sudo hostapd conf/hostapd.conf &
+sleep 1
 
-# Run access point daemon
-hostapd conf/hostapd.conf
 
-# Stop
+echo -e "Starting daemon dnsmasq... "
+# start dnsmasq server (see dnsmasq.conf) -7 /etc/dnsmasq.d
+sudo dnsmasq -x /var/run/dnsmasq.pid -C conf/dnsmasq.conf
+sleep 1
 
-# Disable NAT
-iptables -D POSTROUTING -t nat -o ens33 -j MASQUERADE
+echo -e "Starting daemon dhcpd... "
+# start or resart dhcpd server (see dhcpd.conf)
+sudo touch /var/lib/dhcp/dhcpd.leases
+sudo dhcpd  -f -pf /var/run/dhcp-server/dhcpd.pid -cf conf/dhcpd.conf $INT_WIFI &
+sleep 2
 
-# Disable routing
-sysctl net.ipv4.ip_forward=0
+#  Turn on IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Disable DHCP/DNS server
-service dnsmasq stop
-service hostapd stop
+echo -e "IPTABLES configuration"
+# IP TABLES
+sudo modprobe ipt_MASQUERADE
+sudo iptables -A POSTROUTING -t nat -o $INT_NET -j MASQUERADE
+sudo iptables -A FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT
+sudo iptables -A FORWARD -i $INT_WIFI --destination $SUBNET --match state --state NEW --jump ACCEPT
+sudo iptables -A INPUT -s $SUBNET --jump ACCEPT
+
+
+echo -e "[finished! Please don't close the shell ]"
+echo -e "[ENTER = STOP hostapd dhcpd dnsmasq   ]"
+echo -e "[        STOP interface wifi     ]"
+echo -e "[        ERRASE IPTABLES rules   ]"
+
+echo -e "Stop hostapd, dhcpd, dnsmasq & wifi interface..."
+# kill hostapd, dnsmasq & dhcpd
+sudo killall hostapd dnsmasq dhcpd
+
+echo -e "errasing IPTABLES RULES..."
+sudo iptables -D POSTROUTING -t nat -o $INT_NET -j MASQUERADE 2>/dev/null
+sudo iptables -D FORWARD -i $INT_WIFI --destination $SUBNET --match state --state NEW --jump ACCEPT 2>/dev/null
+sudo iptables -D FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT 2>/dev/null
+sudo iptables -D INPUT -s $SUBNET --jump ACCEPT 2>/dev/null
+
+echo -e "iptables CLEAN"
+
+# interfaces
+sudo ifconfig $INT_WIFI down
+sudo ifconfig $INT_WIFI up
+ 
+# Turn off IP forwarding
+echo 0 > /proc/sys/net/ipv4/ip_forward
+echo -e "Done!"
